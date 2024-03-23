8c6a907e87506949c9bebfa3f132fd70525676c9
PhuNguyen182 <52516080+PhuNguyen182@users.noreply.github.com>
2024-03-23 23:50:14 +0700

    Re-import assets

--- Assets/Bubble Shooter/Scripts/Gameplay/Game Entities/BallMovement.cs
+++ Assets/Bubble Shooter/Scripts/Gameplay/Game Entities/BallMovement.cs
@@ -6,7 +6,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace BubbleShooter.Scripts.Gameplay.GameEntities.CustomEntities
+namespace BubbleShooter.Scripts.Gameplay.GameEntities
 {
     public class BallMovement : MonoBehaviour, IFixedUpdateHandler, IBallMovement
     {
