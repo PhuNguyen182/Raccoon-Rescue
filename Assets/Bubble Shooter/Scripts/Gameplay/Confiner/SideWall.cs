c96179c709f5dfc04cfb6a0469eb07d6292c4dac
PhuNguyen182 <52516080+PhuNguyen182@users.noreply.github.com>
2024-03-24 00:20:37 +0700

    Update wall prefab, update ball movement

--- Assets/Bubble Shooter/Scripts/Gameplay/Confiner/SideWall.cs
+++ Assets/Bubble Shooter/Scripts/Gameplay/Confiner/SideWall.cs
@@ -1,18 +1,38 @@
+using System.Threading;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using BubbleShooter.Scripts.Common.Enums;
+using Cysharp.Threading.Tasks;
 
 namespace BubbleShooter.Scripts.Gameplay.Confiner
 {
     public class SideWall : MonoBehaviour
     {
         [SerializeField] private WallSide wallSide;
+        [SerializeField] private Collider2D wallCollider;
         [SerializeField] private Camera mainCamera;
+
+        private CancellationToken _token;
 
         private void Awake()
         {
+            _token = this.GetCancellationTokenOnDestroy();
+
+            wallCollider.enabled = false;
             RestrictPosition();
+        }
+
+        private void Start()
+        {
+            ControllWallCollider().Forget();
+        }
+
+        public async UniTaskVoid ControllWallCollider()
+        {
+            wallCollider.enabled = false;
+            await UniTask.DelayFrame(2, PlayerLoopTiming.Update, _token);
+            wallCollider.enabled = true;
         }
 
         private void RestrictPosition()
--- Assets/Bubble Shooter/Scripts/Gameplay/Game Entities/BallMovement.cs
+++ Assets/Bubble Shooter/Scripts/Gameplay/Game Entities/BallMovement.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using Scripts.Common.UpdateHandlerPattern;
 using BubbleShooter.Scripts.Common.Interfaces;
@@ -13,6 +14,12 @@ namespace BubbleShooter.Scripts.Gameplay.GameEntities
         [SerializeField] private Rigidbody2D ballBody;
         [SerializeField] private Collider2D ballCollider;
 
+        [Header("Check Reflection")]
+        [Tooltip("This value is used to delay reflection checking when the ball hit the wall and bounce again")]
+        [SerializeField] private int delayFrame = 3;
+        [SerializeField] private float ballRadius = 0.3f;
+        [SerializeField] private float ballDistance = 1f;
+
         [Header("Check Layer Maskes")]
         [SerializeField] private LayerMask ceilMask;
         [SerializeField] private LayerMask ballMask;
@@ -20,6 +27,9 @@ namespace BubbleShooter.Scripts.Gameplay.GameEntities
 
         private float _ballSpeed = 0;
         private bool _canMove = false;
+        private bool _isReflect = false;
+
+        private CancellationToken _token;
         private Vector2 _moveDirection = Vector2.zero;
 
         public bool CanMove
@@ -32,6 +42,11 @@ namespace BubbleShooter.Scripts.Gameplay.GameEntities
             }
         }
 
+        private void Awake()
+        {
+            _token = this.GetCancellationTokenOnDestroy();
+        }
+
         private void OnEnable()
         {
             UpdateHandlerManager.Instance.AddFixedUpdateBehaviour(this);
@@ -42,6 +57,23 @@ namespace BubbleShooter.Scripts.Gameplay.GameEntities
             if (_canMove)
             {
                 MoveBall();
+                CheckReflection().Forget();
+            }
+        }
+
+        private async UniTaskVoid CheckReflection()
+        {
+            RaycastHit2D hitInfor = Physics2D.CircleCast(transform.position, ballRadius, transform.up, ballDistance, reflectMask);
+
+            if (hitInfor && !_isReflect)
+            {
+                _isReflect = true;
+                Vector2 hitNormal = hitInfor.normal;
+                Vector2 newDirection = Vector2.Reflect(_moveDirection, hitNormal);
+                _moveDirection = newDirection.normalized;
+
+                await UniTask.DelayFrame(4, delayTiming: PlayerLoopTiming.FixedUpdate, cancellationToken: _token);
+                _isReflect = false;
             }
         }
 
