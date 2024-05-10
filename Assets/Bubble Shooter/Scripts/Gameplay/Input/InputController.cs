75ed50d75c11ae97794bfd17e436a48b7d866f7b
PhuNguyen182 <52516080+PhuNguyen182@users.noreply.github.com>
2024-05-11 00:48:11 +0700

    Add input system

---
+++ Assets/Bubble Shooter/Scripts/Gameplay/Input/InputController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace BubbleShooter.Scripts.Gameplay.Input
+{
+    public class InputController : MonoBehaviour
+    {
+        private GameplayInput _inputController;
+
+        public bool Release { get; private set; }
+        public Vector2 Position { get; private set; }
+
+        private void Awake()
+        {
+            _inputController = new();
+
+            _inputController.Gameplay.Move.started += OnMovePerform;
+            _inputController.Gameplay.Move.performed += OnMovePerform;
+            _inputController.Gameplay.Move.canceled += OnMovePerform;
+
+            _inputController.Gameplay.Release.started += OnReleasePerform;
+            _inputController.Gameplay.Release.performed += OnReleasePerform;
+            _inputController.Gameplay.Release.canceled += OnReleasePerform;
+        }
+
+        private void OnReleasePerform(InputAction.CallbackContext context)
+        {
+            Release = context.ReadValue<bool>();
+        }
+
+        private void OnMovePerform(InputAction.CallbackContext context)
+        {
+            Position = context.ReadValue<Vector2>();
+        }
+
+        private void OnEnable()
+        {
+            _inputController.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _inputController.Disable();
+        }
+
+        private void OnDestroy()
+        {
+            _inputController.Dispose();
+        }
+    }
+}
