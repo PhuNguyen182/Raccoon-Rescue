87cb7f3c22a56605e3a7651559cbf88e2c4bbbf7
PhuNguyen182 <52516080+PhuNguyen182@users.noreply.github.com>
2024-05-12 23:54:14 +0700

    Use trick to fix bug

--- Assets/Bubble Shooter/Scripts/Gameplay/Confiner/SideWall.cs
+++ Assets/Bubble Shooter/Scripts/Gameplay/Confiner/SideWall.cs
@@ -1,46 +1,16 @@
-using System.Threading;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using BubbleShooter.Scripts.Common.Enums;
-using Cysharp.Threading.Tasks;
 
 namespace BubbleShooter.Scripts.Gameplay.Confiner
 {
     public class SideWall : MonoBehaviour
     {
-        [SerializeField] private WallSide wallSide;
-        [SerializeField] private Collider2D wallCollider;
-        [SerializeField] private Camera mainCamera;
+        [SerializeField] private GameObject fadeObject;
 
-        private CancellationToken _token;
-
-        private void Awake()
+        public void ShowReflectWall(bool active)
         {
-            _token = this.GetCancellationTokenOnDestroy();
-
-            wallCollider.enabled = false;
-            RestrictPosition();
-        }
-
-        private void Start()
-        {
-            ControllWallCollider().Forget();
-        }
-
-        public async UniTaskVoid ControllWallCollider()
-        {
-            wallCollider.enabled = false;
-            await UniTask.DelayFrame(2, PlayerLoopTiming.Update, _token);
-            wallCollider.enabled = true;
-        }
-
-        private void RestrictPosition()
-        {
-            Vector3 position = wallSide == WallSide.Left ? mainCamera.ViewportToWorldPoint(new Vector3(0, 0.5f))
-                                                         : mainCamera.ViewportToWorldPoint(new Vector3(1, 0.5f));
-            position.z = 0;
-            transform.position = position;
+            fadeObject.SetActive(active);
         }
     }
 }
