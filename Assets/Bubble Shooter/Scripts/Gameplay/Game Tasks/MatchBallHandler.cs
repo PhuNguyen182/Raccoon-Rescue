f50252913dec78c7cf794d645386ba61759e32ce
PhuNguyen182 <52516080+PhuNguyen182@users.noreply.github.com>
2024-06-02 20:21:50 +0700

    Fix check target bug, update simple pool

--- Assets/Bubble Shooter/Scripts/Gameplay/Game Tasks/MatchBallHandler.cs
+++ Assets/Bubble Shooter/Scripts/Gameplay/Game Tasks/MatchBallHandler.cs
@@ -94,7 +94,7 @@ namespace BubbleShooter.Scripts.Gameplay.GameTasks
                     _checkBallClusterTask.CheckFreeCluster();
                 }
 
-                //else
+                if(!_checkTargetTask.IsTargetAdded)
                 {
                     await UniTask.Delay(TimeSpan.FromSeconds(0.3f), cancellationToken: _token);
                     _checkTargetTask.CheckTarget();
